e5a44645e4600cc0bc179e4fcdd81a27bbe658f9
samir 9187 <samirkunwar9187@gmail.com>
2024-11-12 10:57:27 +0530

    quot error fixed and link navigation fixed

--- about/src/app/components/Mission.tsx
+++ about/src/app/components/Mission.tsx
@@ -9,16 +9,16 @@ const Mission: React.FC = () => {
         </h2>
         <div className="mt-8 p-8 bg-white dark:bg-gray-800 rounded-lg shadow-lg">
           <p className="mt-6 text-lg text-galaxy dark:text-purple-400 font-medium leading-relaxed tracking-wide text-center">
-            "To create innovative and efficient hiring solutions that enable
-            businesses to find top-tier talent using AI-driven insights. By
-            leveraging cutting-edge technologies and data analytics, we aim to
-            streamline the recruitment process, reduce hiring biases, and
+            &quot;To create innovative and efficient hiring solutions that
+            enable businesses to find top-tier talent using AI-driven insights.
+            By leveraging cutting-edge technologies and data analytics, we aim
+            to streamline the recruitment process, reduce hiring biases, and
             improve candidate matching. Our solutions empower organizations to
             make data-informed decisions, ensuring they hire the right talent
             quickly and effectively. With a focus on automation, personalized
             candidate experiences, and scalable recruitment strategies, we aim
             to transform the hiring landscape and support businesses in building
-            diverse and high-performing teams."
+            diverse and high-performing teams.&quot;
           </p>
         </div>
       </div>
