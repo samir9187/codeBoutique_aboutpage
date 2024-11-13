4c30e5a5cb366e3ebd876fcf1bf3c2fdd60530e6
samir 9187 <samirkunwar9187@gmail.com>
2024-11-13 17:07:05 +0530

    some change

--- about/src/app/components/Hero.tsx
+++ about/src/app/components/Hero.tsx
@@ -8,14 +8,14 @@ const HeroSection: React.FC = () => {
           {/* Text Section */}
           <div className="md:mr-12 text-center md:text-left mt-8 md:mt-0">
             <p className="text-transparent bg-clip-text bg-[linear-gradient(to_right,_#000_0%,_#3c3cbf_50%)] dark:bg-[linear-gradient(to_right,_#3c3cbf_0%,_#FFFFFF_50%)] font-bold text-3xl md:text-4xl lg:text-5xl xl:text-6xl break-words">
-              GalaxyCorp empowers growth with intelligent hiring solutions.
+            GalaxyCorp advances space exploration with cutting-edge research and innovative solutions.
             </p>
           </div>
 
           {/* Image Section */}
           <div className="flex justify-center md:flex-col md:items-center gap-6">
             {/* Primary Image (Visible on all screen sizes) */}
-            <div className="w-48 h-48 md:w-64 md:h-64 lg:w-80 lg:h-80 transform rotate-45 border-4 border-blue-500 dark:border-blue-500 overflow-hidden">
+            <div className="w-48 h-48 md:w-64 md:h-64 lg:w-80 lg:h-80 transform rotate-45 border-4  dark:border-blue-500 overflow-hidden">
               <img
                 alt="GalaxyCorp empowers growth with intelligent hiring solutions."
                 className="w-full h-full object-cover"
@@ -24,7 +24,7 @@ const HeroSection: React.FC = () => {
             </div>
 
             {/* Secondary Image (Visible only on medium screens and larger) */}
-            <div className="hidden md:block w-32 h-32 lg:w-48 lg:h-48 xl:w-64 xl:h-64 transform rotate-45 border-4 border-blue-500 dark:border-blue-500 overflow-hidden">
+            <div className="hidden md:block w-32 h-32 lg:w-48 lg:h-48 xl:w-64 xl:h-64 transform rotate-45 border-4 dark:border-blue-500 overflow-hidden">
               <img
                 alt="GalaxyCorp empowers growth with intelligent hiring solutions."
                 className="w-full h-full object-cover"
